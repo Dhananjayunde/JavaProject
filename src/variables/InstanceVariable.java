663e06cfc6444484a8ea6fffb613b996d239152e
Dhananjayunde <77325569+Dhananjayunde@users.noreply.github.com>
2025-08-29 15:46:02 +0530

    changes for variable concepts

--- src/variables/InstanceVariable.java
+++ src/variables/InstanceVariable.java
@@ -10,3 +10,14 @@ int a=10;  // instance variable declare outside method inside class
 		obj.method();
 	}
 }
+
+/*
+ *   A variable declared inside the class but outside the body of the method, is called an instance variable. It is not declared as static.
+ 
+ 	1. Each object gets its own copy.
+    2.Instance variables belong to an object (not the class itself). When we create multiple objects of the same class, each object gets a separate copy of the instance variables.
+    3.Has default values (e.g., int → 0, String → null).
+    4.Not shared among multiple objects.
+    5. Each object maintains its own unique copy of the instance variables. Changes made to one object's instance variable do not affect the instance variables of another object.
+
+ */
--- src/variables/LocalVariables.java
+++ src/variables/LocalVariables.java
@@ -1,13 +1,22 @@
 package variables;  // package 
 
-public class LocalVariables { // <- Open class block-->                 // class classname
+public class LocalVariables {     
 
-	void method() { //<--open method block
+	void method() {
 		int a=10; //Local variable declare inside method , method declare inside class
 		System.out.println(a);                                                          
-	} //<--- closed method block
+	} 
 	public static void main(String[] args) {
 		LocalVariables obj =new LocalVariables();
 		obj.method();
 	}
-} //<--- // <- close class block-->
+} 
+
+/*
+A variable declared inside a method is called a local variable. or
+A variable declared inside the body of the method is called local variable. 
+You can use this variable only within that method and the other methods in the class aren't even aware that the variable exists.
+1.	Available only inside the method where it is declared.
+2.	Must be initialized before use.
+3.	Cannot be static.
+*/
